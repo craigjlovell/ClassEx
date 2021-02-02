c4c3c3425957e47f33adea16840c733260650f55
Craig Lovell <s171627@students.aie.edu.au>
2021-02-02 16:53:02 +1100

    change

---
+++ AIE03Circle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AIE03Circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double rad = 8;
+            double cirm = 2 * Math.PI * rad;
+            double area = ;
+
+
+        }
+    }
+}
