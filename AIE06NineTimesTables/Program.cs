3ad01963133902d36fb94191942bb506f148394e
Craig Lovell <s171627@students.aie.edu.au>
2021-02-03 13:53:32 +1100

    Updated

--- AIE06NineTimesTables/Program.cs
+++ AIE06NineTimesTables/Program.cs
@@ -6,7 +6,14 @@ namespace AIE06NineTimesTables
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sum = 0;
+            for (int i = 1; i < 11; i++)
+            {
+                sum = i * 9;
+                Console.WriteLine($"The sum is {sum}");
+            }
+
+
         }
     }
 }
